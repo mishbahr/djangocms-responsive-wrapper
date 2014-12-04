2bfab6456aaed2d9b396cc9fe9d42e898eb1b9a8
Mishbah <mishbah@jp74.com>
2014-12-04 00:53:24 +0000

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -14,6 +14,7 @@ try:
         'DATABASES': {
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'responsive',
             }
         },
         'ROOT_URLCONF': 'responsive_wrapper.urls',
